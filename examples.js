83e47c6be1f65d58af28e165fdc10fcb158f707d
Anthony Tietjen <tietjen.anthony@gmail.com>
2021-05-14 00:02:20 -0600

    Added graphs in comments

--- examples.js
+++ examples.js
@@ -1,4 +1,8 @@
 // O(n)  - Linear
+// |         /
+// |      /    
+// |   /
+// |/___________
 function addLinear(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
@@ -10,13 +14,21 @@ console.log(addLinear(100000));
 
 
 // O(1) - Constant
+// |
+// |
+// |-----------
+// |___________
 function addConstant(n) {
   let total = (n * (n + 1)) / 2;
   return total;
 }
 console.log(addConstant(100000));
 
-// O(n²) Quadratic
+// O(n^2) Quadratic
+// |  |
+// |  /   
+// | / 
+// |/__________
 function addQuadratic(n) {
   let total = 0;
   for (let i = 1; i <= n; i++) {
@@ -27,3 +39,8 @@ function addQuadratic(n) {
   return total;
 }
 console.log(addQuadratic(1000));
+
+// Simplifying
+// O(n + 10)         = O(n)
+// O(100)            = O(1)
+// O(n^2 + 10n + 8)  = O(n^2)
